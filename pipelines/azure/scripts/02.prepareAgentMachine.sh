2387667e9cc0bb32bd63cff98ae0be9ea1d3baff
Mihai Ungureanu <mihai.ungureanu@softwareag.com>
2022-12-27 11:49:46 +0100

    More details on the lock management

--- pipelines/azure/scripts/02.prepareAgentMachine.sh
+++ pipelines/azure/scripts/02.prepareAgentMachine.sh
@@ -5,68 +5,48 @@
 # Adding some logs for debugging, looking at file
 # /var/lib/dpkg/lock-frontend
 
-echo "lslocks"
-lslocks
+checkLock(){
+  echo "######### Checking apt lock"
+  echo "######### lslocks | grep '/var/lib/dpkg/lock-frontend'"
+  lslocks | grep '/var/lib/dpkg/lock-frontend'
+  if ( lslocks | grep '/var/lib/dpkg/lock-frontend' ) ; then
+    echo "######### File /var/lib/dpkg/lock-frontend already locked!"
+    echo "######### lslocks"
+    lslocks
+    echo "######### sudo ps aux | grep -i apt"
+    sudo ps aux | grep -i apt
+    echo "######### sudo lsof /var/lib/dpkg/lock-frontend"
+    sudo lsof /var/lib/dpkg/lock-frontend
+    echo "######### Seeping a bit..."
+    sleep 15
+  else
+    echo "######### apt lock is not present, continuing..."
+  fi  
+}
 
 
-echo ">>>>>>>>>>>>>>>>Updating OS software"
+echo ">>>>>>>>>>>>>>>> Updating OS software"
+checkLock
 sudo apt -y update
 
-if [ -f /var/lib/dpkg/lock-frontend ]; then
-  echo "File /var/lib/dpkg/lock-frontend exists!"
-  echo "lslocks"
-  lslocks
-  echo "sudo ps aux | grep -i apt"
-  sudo ps aux | grep -i apt
-  echo "sudo lsof /var/lib/dpkg/lock-frontend"
-  sudo lsof /var/lib/dpkg/lock-frontend
-  echo "Seeping a bit..."
-  sleep 15
-fi
-
-echo ">>>>>>>>>>>>>>>>Updating base libraries..."
+echo ">>>>>>>>>>>>>>>> Updating base libraries..."
+checkLock
 sudo apt-get -qq -y update
 
-if [ -f /var/lib/dpkg/lock-frontend ]; then
-  echo "File /var/lib/dpkg/lock-frontend exists!"
-  echo "sudo ps aux | grep -i apt"
-  sudo ps aux | grep -i apt
-  echo "sudo lsof /var/lib/dpkg/lock-frontend"
-  sudo lsof /var/lib/dpkg/lock-frontend
-  echo "Seeping a bit..."
-  sleep 15
-fi
-
-echo ">>>>>>>>>>>>>>>>Installing prerequisites..."
+echo ">>>>>>>>>>>>>>>> Installing prerequisites..."
+checkLock
 sudo apt-get -qq install -y ca-certificates curl gnupg2 fuse-overlayfs
 . /etc/os-release
 echo "Installing buildah for OS release ${VERSION_ID}..."
 echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 curl -fsL "https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add - &&
 
-if [ -f /var/lib/dpkg/lock-frontend ]; then
-  echo "File /var/lib/dpkg/lock-frontend exists!"
-  echo "sudo ps aux | grep -i apt"
-  sudo ps aux | grep -i apt
-  echo "sudo lsof /var/lib/dpkg/lock-frontend"
-  sudo lsof /var/lib/dpkg/lock-frontend
-  echo "Seeping a bit..."
-  sleep 15
-fi
-
+echo ">>>>>>>>>>>>>>>> Updating base libraries again..."
+checkLock
 sudo apt-get -qq -y update
 
-if [ -f /var/lib/dpkg/lock-frontend ]; then
-  echo "File /var/lib/dpkg/lock-frontend exists!"
-  echo "sudo ps aux | grep -i apt"
-  sudo ps aux | grep -i apt
-  echo "sudo lsof /var/lib/dpkg/lock-frontend"
-  sudo lsof /var/lib/dpkg/lock-frontend
-  echo "Seeping a bit..."
-  sleep 15
-fi 
-
-echo ">>>>>>>>>>>>>>>>Installing buildah..."
-
+echo ">>>>>>>>>>>>>>>> Installing buildah..."
+checkLock
 sudo apt-get -qq -y install buildah
-echo "Machine prepared"
+
+echo ">>>>>>>>>>>>>>>> Machine prepared"
